e25151a88394e44f53c2cbe621729ae4056ecd55
Jabulani Kapherachoka <jabulanikbanda769@gmail.com>
2025-04-21 10:42:03 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define VOTER_REG_FILE "voterReg.txt"
+#define CANDIDATE_REG_FILE "candidateReg.txt"
+#define CANDIDATE VOTES_FILE "candidateVotes.txt"
+#define VOTED_ALREADY_FILE "votedAlready.txt"
+
+int main(){
+    
+}
